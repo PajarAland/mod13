94e98f06e4aca5c8fc64c36710261dbd5f88fe41
PajarAland <raymonto08@gmail.com>
2024-05-22 07:44:30 +0700

    first commit

---
+++ mod13_1302223146/Program.cs
@@ -0,0 +1,40 @@
+﻿using mod13_1302223146;
+using System;
+
+namespace MyApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<String> smt;
+            PusatDataSingleton data1 = PusatDataSingleton.GetDataSingleton();
+            PusatDataSingleton data2 = PusatDataSingleton.GetDataSingleton();
+
+            Console.WriteLine("Add dan Menampilkan:");
+            data1.AddSebuahData("Fajar Ramadhan");
+            data1.AddSebuahData("Aldino Rasendriya");
+            data1.AddSebuahData("Harits Azfa");
+            data1.AddSebuahData("Ghifari Permata");
+            data1.AddSebuahData("Fransiskus Xaverius");
+            data1.AddSebuahData("Denanda Permana");
+            data1.AddSebuahData("FAL");
+            data2.printSemuaData();
+            Console.WriteLine();
+
+            Console.WriteLine("Hapus Data:");
+            data2.HapusSuatuData(6);
+            data1.printSemuaData();
+            smt = data1.GetSemuaData();
+            Console.WriteLine();
+
+            Console.WriteLine("Count Data:");
+            smt = data1.GetSemuaData();
+            Console.WriteLine("data1: " + smt.Count);
+            smt = data1.GetSemuaData();
+            Console.WriteLine("data2: " + smt.Count);
+
+
+        }
+    }
+}
---
+++ mod13_1302223146/PusatDataSingleton.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mod13_1302223146
+{
+    public sealed class PusatDataSingleton
+    {
+        List<String> dataTersimpan;
+        static PusatDataSingleton _instance;
+
+        private PusatDataSingleton() 
+        {
+            dataTersimpan = new List<String>();
+        }
+
+        public static PusatDataSingleton GetDataSingleton()
+        {
+            if (_instance == null) 
+            {
+                _instance = new PusatDataSingleton();
+            }
+            return _instance;
+        }
+
+        public List<String> GetSemuaData()
+        {
+            return dataTersimpan;
+        }
+
+        public void printSemuaData()
+        {
+            foreach (String s in dataTersimpan)
+            {
+                System.Console.WriteLine(s);
+            }
+        }
+
+        public void AddSebuahData(String input)
+        {
+            this.dataTersimpan.Add(input);
+        }
+        public void HapusSuatuData(int index)
+        {
+            this.dataTersimpan.RemoveAt(index);
+        }
+    }
+}
